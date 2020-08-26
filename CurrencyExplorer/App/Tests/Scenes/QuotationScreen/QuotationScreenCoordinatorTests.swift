3d25ea2d9a9e0e5f9b8b1df9f1a1e48d499c52aa
Fabricio Dede <dede.exe@gmail.com>
2020-08-26 20:31:19 -0300

    add: QuotationScreenCoordinatorTests

---
+++ CurrencyExplorer/App/Tests/Scenes/QuotationScreen/QuotationScreenCoordinatorTests.swift
@@ -0,0 +1,39 @@
+//
+//  QuotationScreenCoordinatorTests.swift
+//  CurrencyExplorerTests
+//
+//  Created by User on 8/26/20.
+//  Copyright © 2020 User. All rights reserved.
+//
+
+import XCTest
+@testable import CurrencyExplorer
+
+class QuotationScreenCoordinatorTests: XCTestCase {
+
+    let sut = QuotationScreenCoordinator()
+
+    func testHasQuotationScreenViewController() throws {
+        sut.start()
+        XCTAssertNotNil(sut.viewController)
+        XCTAssertTrue(sut.viewController is QuotationScreenViewController)
+    }
+
+    func testInternalView() throws {
+        sut.start()
+        guard let vc = sut.viewController as? QuotationScreenViewController else {
+            XCTFail("Fail extracting viewcontroller")
+            return
+        }
+
+        XCTAssertTrue(vc.internalView is QuotationScreenView)       //Yes. It's obvious, but somebody still can change it in code.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
